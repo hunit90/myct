93485de35bf642ec06b116a91f05342a4dcd1998
hunit <hunit612@naver.com>
2023-09-07 23:55:12 +0900

    refactor: ormconfig 수정

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,26 @@
-module.exports = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities:
-    process.env.NODE_ENV === 'dev' ? ['**/*.entity.js'] : ['**/*.entity.js'],
+const dbConfig = {
   synchronize: false,
 };
+
+switch (process.env.NODE_ENV) {
+  case 'dev':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    break;
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
