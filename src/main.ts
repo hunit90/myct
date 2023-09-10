d991a49ad300875b4575e65e2a950ddee815c95a
hunit <hunit612@naver.com>
2023-09-10 23:50:33 +0900

    preproduction commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupApp } from './setup-app';
-import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    
-  );
-  setupApp(app);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
